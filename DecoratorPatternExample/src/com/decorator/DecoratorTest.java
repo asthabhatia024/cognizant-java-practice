4015b9388bedb86e42c1b811658ab1cd2c8198cc
Aastha <asthabhatia024@gmail.com>
2026-06-29 13:46:00 +0530

    Added Decorator pattern

---
+++ DecoratorPatternExample/src/com/decorator/DecoratorTest.java
@@ -0,0 +1,16 @@
+package com.decorator;
+
+public class DecoratorTest {
+
+    public static void main(String[] args) {
+
+        Notifier notifier = new EmailNotifier();
+
+        notifier = new SMSNotifierDecorator(notifier);
+
+        notifier = new SlackNotifierDecorator(notifier);
+
+        notifier.send("Design Pattern Assignment Completed!");
+
+    }
+}
---
+++ DecoratorPatternExample/src/com/decorator/EmailNotifier.java
@@ -0,0 +1,10 @@
+package com.decorator;
+
+public class EmailNotifier implements Notifier {
+
+    @Override
+    public void send(String message) {
+        System.out.println("Sending Email: " + message);
+    }
+
+}
